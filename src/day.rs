9bba739dc8d70688c77cdfbede109e5d3bdc164a
Chris Paterson <chris@chrispaterson.co.uk>
2025-12-01 13:43:26 +0000

    Handle errors

--- src/day.rs
+++ src/day.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use std::time::{Duration, Instant};
 
 pub struct ExecutionResult<O: std::fmt::Display + Eq> {
@@ -52,51 +53,51 @@ pub trait DayImplementation {
     fn example_part_1_result(&self) -> Self::Output<'static>;
     fn example_part_2_result(&self) -> Self::Output<'static>;
 
-    fn execute_part_1<'a>(&self, input: &'a str) -> (Self::Output<'a>, Self::Context<'a>);
-    fn execute_part_2<'a>(&self, input: &'a str, context: Self::Context<'a>) -> Self::Output<'a>;
+    fn execute_part_1<'a>(&self, input: &'a str) -> Result<(Self::Output<'a>, Self::Context<'a>)>;
+    fn execute_part_2<'a>(&self, input: &'a str, context: Self::Context<'a>) -> Result<Self::Output<'a>>;
 
-    fn run_with_input<'a>(&self, input: &'a str) -> ExecutionResult<Self::Output<'a>> {
+    fn run_with_input<'a>(&self, input: &'a str) -> Result<ExecutionResult<Self::Output<'a>>> {
         log::debug!("Starting part 1 for day {}", self.day());
         let start_part_1 = Instant::now();
-        let (part_1_result, context) = self.execute_part_1(input);
+        let (part_1_result, context) = self.execute_part_1(input)?;
         let part_1_time = start_part_1.elapsed();
         log::info!("Part 1 completed in {:?}, result: {}", part_1_time, part_1_result);
 
         log::debug!("Starting part 2 for day {}", self.day());
         let start_part_2 = Instant::now();
-        let part_2_result = self.execute_part_2(input, context);
+        let part_2_result = self.execute_part_2(input, context)?;
         let part_2_time = start_part_2.elapsed();
         log::info!("Part 2 completed in {:?}, result: {}", part_2_time, part_2_result);
 
-        ExecutionResult {
+        Ok(ExecutionResult {
             part_1_result,
             part_1_time,
             part_2_result,
             part_2_time
-        }
+        })
     }
 }
 
 pub trait Day {
     fn day(&self) -> u8;
 
-    fn test_day(&self) -> TestResult;
-    fn execute_day(&self) -> DayResult;
+    fn test_day(&self) -> Result<TestResult>;
+    fn execute_day(&self) -> Result<DayResult>;
 }
 
 impl<T: DayImplementation> Day for T {
     fn day(&self) -> u8 { DayImplementation::day(self) }
 
-    fn test_day(&self) -> TestResult {
+    fn test_day(&self) -> Result<TestResult> {
         log::info!("Running tests for day {}", self.day());
-        let result = self.run_with_input(DayImplementation::example_input(self));
-        TestResult::from_execution_result(result, DayImplementation::example_part_1_result(self), DayImplementation::example_part_2_result(self))
+        let result = self.run_with_input(DayImplementation::example_input(self))?;
+        Ok(TestResult::from_execution_result(result, DayImplementation::example_part_1_result(self), DayImplementation::example_part_2_result(self)))
     }
 
-    fn execute_day(&self) -> DayResult {
+    fn execute_day(&self) -> Result<DayResult> {
         // Temp hack before we implement input fetching
         let input = self.example_input();
         log::info!("Executing day {} with actual input", self.day());
-        DayResult::from(self.run_with_input(input))
+        Ok(DayResult::from(self.run_with_input(input)?))
     }
 }
